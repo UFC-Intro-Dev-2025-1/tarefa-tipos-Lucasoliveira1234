bad82c06fbc1262c8486d5653d941c1808317a20
Lucas Sousa Oliveira <ls5105254@gmail.com>
2025-05-23 22:40:18 +0000

    Solução da tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-// Escreva seu código aqui!
+//Tipo number
+let NumeroDeMouses: number = 30;
+//Tipo string
+let NomeDaProfessora: string = "Lana";
+//Tipo boolean
+let ProfessoraEmSala: boolean = true;
+//Tipo undefined
+let MinhaMedia: string | undefined;
+
+console.log("Número de mouses:", NumeroDeMouses);
+console.log( "Nome da professora:", NomeDaProfessora);
+console.log("Professora em sala:", ProfessoraEmSala);
+console.log("Minha média:", MinhaMedia);
